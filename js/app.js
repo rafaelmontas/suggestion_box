fe8c0b39c6beab475931c81a5261fbb71f04fba1
Rafael Montas <rafaelmontas1@gmail.com>
2016-08-12 20:52:09 -0400

    comment

--- js/app.js
+++ js/app.js
@@ -6,6 +6,10 @@ app.config(function($routeProvider) {
       controller: "HomeController",
       templateUrl: "views/home.html"
     })
+    .when("/suggestion/:id", {
+    	controller: "SuggestionController",
+    	templateUrl: "views/suggestion.html"
+    })
     .otherwise({
     redirectTo: "/"
   });
