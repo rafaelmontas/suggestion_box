18b46357c2b33b8acaf149adf9da1a5cf63f7e9c
Rafael Montas <rafaelmontas1@gmail.com>
2016-08-16 22:25:24 -0400

    Finally!!!!

--- js/controllers/SuggestionController.js
+++ js/controllers/SuggestionController.js
@@ -1,6 +1,6 @@
 app.controller("SuggestionController", ["$scope", "$routeParams", "suggestions", function($scope, $routeParams, suggestions) {
 	$scope.post = suggestions.posts[$routeParams.id];
-	
+
 
 	$scope.addComment = function() {
 		if(!$scope.new_comment || $scope.new_comment === "") {
@@ -17,5 +17,6 @@ app.controller("SuggestionController", ["$scope", "$routeParams", "suggestions",
 	$scope.upVoteComment = function(comment) {
 		comment.upvotes += 1;
 	};
-}]);
+}]); 
+
 
